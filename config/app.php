7acb8c0252a632b86ce422a9723c847c44eca4e2
dennisQ23 <dnnsqltn@yahoo.com>
2020-04-30 02:02:41 +0800

    change key

--- config/app.php
+++ config/app.php
@@ -119,7 +119,8 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    // 'key' => env('APP_KEY'),
+    'key' => 'nLph4Uq2dT3slGTqT9fp4DYe2tXkYXAHil/bOQz13ag',
 
     'cipher' => 'AES-256-CBC',
 
